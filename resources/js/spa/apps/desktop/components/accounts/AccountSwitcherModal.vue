d0c494ca441e9b0cea2b4565c69dd560881c8a95
wiynhn <wytucsmb@gmail.com>
2026-08-26 01:05:17 +0630

    udpate

--- resources/js/spa/apps/desktop/components/accounts/AccountSwitcherModal.vue
+++ resources/js/spa/apps/desktop/components/accounts/AccountSwitcherModal.vue
@@ -47,7 +47,7 @@
     import PrimaryIconButton from '@D/components/inter-ui/buttons/PrimaryIconButton.vue';
     import PrimaryPillButton from '@D/components/inter-ui/buttons/PrimaryPillButton.vue';
     import LinkedAccountItem from '@D/components/accounts/parts/LinkedAccountItem.vue';
-    import PrimarySpinAnimation from '@D/components/general/PrimarySpinAnimation.vue';
+    import PrimarySpinAnimation from '@D/components/general/animations/PrimarySpinAnimation.vue';
     import Border from '@D/components/general/Border.vue';
 
     export default defineComponent({
