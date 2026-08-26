5dc268bf4a5b7d0518b6b3cebf6b908c82736e90
wiynhn <wytucsmb@gmail.com>
2026-08-26 10:58:57 +0630

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,9 @@
 # Route crash မဖြစ်အောင် cache အစား clear ပဲ အရင်လုပ်ပါ
 php artisan config:clear
 php artisan route:clear
+php artisan optimize:clear
 php artisan view:clear
-
+php artisan cache:clear
 # Database migration (Database ချိတ်ဆက်ပြီးမှ အလုပ်လုပ်မည်)
 php artisan migrate --force
 
