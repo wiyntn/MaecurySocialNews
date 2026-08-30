ddd92fd6c780d1178180593d56283f819781945c
wiynhn <wytucsmb@gmail.com>
2026-08-31 03:46:41 +0630

    update

--- resources/js/spa/kernel/websockets/index.js
+++ resources/js/spa/kernel/websockets/index.js
@@ -1,38 +1,42 @@
 import Echo from 'laravel-echo';
-
 import Pusher from 'pusher-js';
+
 window.ColibriBRConnected = false;
 window.Pusher = Pusher;
 window.Echo = Echo;
-Pusher.logToConsole = import.meta.env.PUSHER_DEBUG_CONSOLE;
-const REVERB_CONNECTION_STATUS = import.meta.env.VITE_REVERB_CONNECTION_STATUS;
+
+Pusher.logToConsole =
+    import.meta.env.VITE_PUSHER_DEBUG_CONSOLE === 'true';
 
 try {
-    if (REVERB_CONNECTION_STATUS == 'on') {
-        window.ColibriBRD = new Echo({
-            namespace: 'null',
-            broadcaster: 'reverb',
-            key: import.meta.env.VITE_REVERB_APP_KEY,
-            wsHost: import.meta.env.VITE_REVERB_HOST,
-            wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-            wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-            forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-            enabledTransports: ['ws', 'wss'],
-            cluster: false,
-        });
+    window.ColibriBRD = new Echo({
+        broadcaster: 'pusher',
 
-        window.ColibriBRD.connector.pusher.connection.bind('connected', function() {
+        key: import.meta.env.VITE_PUSHER_APP_KEY,
+
+        cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+
+        forceTLS: true,
+
+        enabledTransports: ['ws', 'wss'],
+    });
+
+    window.ColibriBRD.connector.pusher.connection.bind(
+        'connected',
+        function () {
             console.log('📶 Websockets connection is established.');
 
             window.ColibriBRConnected = true;
-        });
-    }
+        }
+    );
 
-    else {
-        console.info("📶 Websockets connection is disabled. Please configure your broadcaster server and enable Reverb connection in your app settings. (ColibriPlus)");
-    }
-}
+    window.ColibriBRD.connector.pusher.connection.bind(
+        'error',
+        function (error) {
+            console.error('❌ Pusher connection error:', error);
+        }
+    );
 
-catch (error) {
-    console.log(error);
+} catch (error) {
+    console.error('❌ Websocket initialization error:', error);
 }
