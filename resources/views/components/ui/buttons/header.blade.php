2367fe88f5a5302f1eb48817b8a6efd4cbc20d9e
wiynhn <wytucsmb@gmail.com>
2026-08-31 06:32:52 +0630

    udpate

--- resources/views/components/ui/buttons/header.blade.php
+++ resources/views/components/ui/buttons/header.blade.php
@@ -8,7 +8,7 @@
 <a {{ $attributes }} href="{{ $url }}" class="shrink-0 cursor-pointer items-center">
     <div class="size-full bg-bg-pr overflow-hidden rounded-full border border-edge-sc">
         @if($avatarUrl)
-            <img class="w-full h-full object-cover" src="{{ $avatarUrl }}" alt="Image">
+            <img class="w-full h-full object-cover" src="{{!! $avatarUrl !!}}" alt="Image">
         @else
             <div class="size-full flex-center text-lab-pr3 hover:text-brand-900">
                 <div class="size-6">
