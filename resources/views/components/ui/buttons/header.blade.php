84fa3f32803f2571a76e37c958a37439248167ac
wiynhn <wytucsmb@gmail.com>
2026-08-31 06:37:46 +0630

    update

--- resources/views/components/ui/buttons/header.blade.php
+++ resources/views/components/ui/buttons/header.blade.php
@@ -6,9 +6,9 @@
 ])
 
 <a {{ $attributes }} href="{{ $url }}" class="shrink-0 cursor-pointer items-center">
-    <div class="size-full bg-bg-pr overflow-hidden rounded-full border border-edge-sc">
+    <div class="size-[40px] rounded-full overflow-hidden border border-bord-pr">
         @if($avatarUrl)
-            <img class="w-full h-full object-cover" src="{!! $avatarUrl !!}" alt="Image">
+            <img class="w-full" src="{! $avatarUrl !}" alt="Image">
         @else
             <div class="size-full flex-center text-lab-pr3 hover:text-brand-900">
                 <div class="size-6">
